5c63963efb14f6a8582d012595d646d01c3bdc82
N.S.Devaraj <nsdevaraj@gmail.com>
2024-07-16 16:48:57 +0530

    speechrec

--- main.js
+++ main.js
@@ -1,68 +1,6 @@
 const textBox = document.getElementById('textBox');
 const errMsg = 'Sorry.. Google Speech API is only supported by Chrome';
-const parser = new UAParser();
-
-// check browser support
-// if (parser.getBrowser().name != 'Chrome') {
-//   alert('Incompatible Browser...');
-//   for (const cssID of [
-//     'loading-icon',
-//     'lang-div',
-//     'shortcut-label',
-//     'toggleBut',
-//   ]) {
-//     document.getElementById(cssID).style.visibility = 'hidden';
-//   }
-//   setTimeout(() => {
-//     textBox.innerText = errMsg;
-//     textBox.style.color = 'white';
-//   }, 1000);
-// }
-
-// PWA
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('sw.js').then(() => { });
-// }
-
-// const langs = [
-//   ['Afrikaans', 'af-ZA'],
-//   ['Bahasa Indonesia', 'id-ID'],
-//   ['Bahasa Melayu', 'ms-MY'],
-//   ['Català', 'ca-ES'],
-//   ['Čeština', 'cs-CZ'],
-//   ['Deutsch', 'de-DE'],
-//   ['English(UK)', 'en-GB'],
-//   ['English(US)', 'en-US'],
-//   ['Español', 'es-ES'],
-//   ['Ελληνικά', 'el-GR'],
-//   ['Euskara', 'eu-ES'],
-//   ['Français', 'fr-FR'],
-//   ['Galego', 'gl-ES'],
-//   ['Hrvatski', 'hr_HR'],
-//   ['IsiZulu', 'zu-ZA'],
-//   ['Íslenska', 'is-IS'],
-//   ['Italiano', 'it-IT'],
-//   ['Magyar', 'hu-HU'],
-//   ['Nederlands', 'nl-NL'],
-//   ['Norsk bokmål', 'nb-NO'],
-//   ['Polski', 'pl-PL'],
-//   ['Português', 'pt-PT'],
-//   ['Română', 'ro-RO'],
-//   ['Slovenčina', 'sk-SK'],
-//   ['Suomi', 'fi-FI'],
-//   ['Svenska', 'sv-SE'],
-//   ['Türkçe', 'tr-TR'],
-//   ['български', 'bg-BG'],
-//   ['Pусский', 'ru-RU'],
-//   ['Српски', 'sr-RS'],
-//   ['한국어', 'ko-KR'],
-//   ['日本語', 'ja-JP'],
-//   ['中文 (台灣)', 'cmn-Hant-TW'],
-//   ['粵語 (香港)', 'yue-Hant-HK'],
-//   ['普通话', 'cmn-Hans-CN'],
-//   ['العربية', 'ar-SA'],
-// ];
-
+ 
 const colorDim = '#9a9a9a';
 const alertStr = `Caution: microphone permission required.
 Please click on the red cross in address bar to allow it.`;
